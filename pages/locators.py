9e744896f681aeeeac212424dfffe04868608f67
owner <bakamaloun@gmail.com>
2020-05-13 13:53:08 +0400

    files for final test

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_MAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASS = (By.CSS_SELECTOR, "#id_login-password")
+    REG_MAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_PASS1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_PASS2 = (By.CSS_SELECTOR, "#id_registration-password2")
